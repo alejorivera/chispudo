283cb700d231a5e9c4f6f96a3f70f50f6a1cf197
Alejo Rivera <alejoriveralara@gmail.com>
2014-09-06 11:15:07 -0600

    ajax working

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,6 @@ class VotesController < ApplicationController
     @post = Post.find(params[:id])
     @vote = @post.votes.create(:user_id => current_user.id, :direction => 1)
     @post.update_attribute :vote_count, @post.vote_count + 1
-    respond_to do |format|
-    	format.js
-    end
   end
   def vote_down
     @post = Post.find(params[:id])
