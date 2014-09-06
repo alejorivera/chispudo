20fc56d707a8dc917f7a816ba2c7823c9dc85354
Alejo Rivera <alejoriveralara@gmail.com>
2014-09-06 15:30:27 -0600

    front end fixes

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20140906193744) do
     t.datetime "updated_at"
   end
 
+  create_table "teams", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
