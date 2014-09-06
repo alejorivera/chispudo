d3107e2541bb37cd1edebcb4f2e95098b6e191a8
Max Holzheu <maxhm10@gmail.com>
2014-09-06 08:35:03 -0600

    post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,5 @@ class Post < ActiveRecord::Base
 	validates :title, presence: true,
  									 length: { maximum: 100 }
 	
-	validates :description, presence: true,
- 									 length: { maximum: 100 } 									 
+	validates :description, length: { maximum: 500 } 									 
 end
